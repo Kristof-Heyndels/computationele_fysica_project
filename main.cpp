3c7dc4743988678a9ebb45cef32eb44e14416381
Kristof <kristof.heynels@student.uhasselt>
2021-04-01 18:06:20 +0000

    fixed boundary and random field picking

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ class Grid {
     int nr_rows_;
     int nr_cols_;
     std::valarray<int> matrix_;
-    std::unordered_map<int, int> eligible_fields_;
+    std::vector<int, int> eligible_fields_;
     void update_tile_weight(const int& pos) {
       if (matrix_[pos] != 9) {
         matrix_[pos]++;
@@ -24,7 +24,7 @@ class Grid {
     std::vector<int> weighted_positions_list() {
       std::vector<int> positions_list;
       for(auto& field: eligible_fields_) {
-        for (int i = 0; i <= field.second; ++i){
+        for (int i = 1; i <= field.second; ++i){
           positions_list.push_back(field.first);
         }
       }
@@ -38,11 +38,6 @@ class Grid {
     }
 
   public:
-    struct position{
-      int row;
-      int col;
-    };
-
     Grid(const int nr_rows, const int nr_cols) :
         nr_rows_ {nr_rows},
         nr_cols_ {nr_cols},
@@ -55,24 +50,24 @@ class Grid {
       matrix_[nr_cols_*row + col] = 9;
       eligible_fields_.erase(nr_cols_*row + col);
 
-      //TODO check edge cases! There is no looparound in a matrix
-      update_tile_weight(nr_cols_*(row-1) + col);
-      update_tile_weight(nr_cols_*row + (col-1));
-      update_tile_weight(nr_cols_*(row+1) + col);
-      update_tile_weight(nr_cols_*row + (col+1));  
+      if (row != 0) { update_tile_weight(nr_cols_*(row-1) + col); }
+      if (col != 0) { update_tile_weight(nr_cols_*row + (col-1)); }
+      if (row != nr_rows_ - 1) {update_tile_weight(nr_cols_*(row+1) + col); }
+      if (col != nr_cols_ - 1) { update_tile_weight(nr_cols_*row + (col+1)); }
+
+
     }
-    position rnd_eligible_field() {
+    int rnd_eligible_field() {
       std::vector<int> positions = weighted_positions_list();
 
       mt19937_64 engine(create_random_seed());
-      auto rnd = bind(uniform_int_distribution<int>(0.0, positions.size()),engine);
+      auto rnd = bind(uniform_int_distribution<int>(0.0, positions.size() - 1),engine);
 
-      auto random_index = rnd();
-      position pos {
-        positions[random_index] / nr_rows_,
-        positions[random_index] % nr_cols_
-      };
-      return pos;
+      return positions[rnd()];
+    }
+    void populate_random_field() {
+      auto rnd_field = rnd_eligible_field();
+      populate_field(rnd_field / nr_cols_, rnd_field % nr_cols_);
     }
 };
 
@@ -91,10 +86,6 @@ void init_grid(Grid& g) {
   int centre_row = g.nr_rows()/2;
   int centre_col = g.nr_cols()/2;
   g.populate_field(centre_row, centre_col);
-  // g.populate_field(centre_row - 1 , centre_col);
-  // g.populate_field(centre_row - 1 , centre_col - 1);
-  // g.populate_field(centre_row - 1 , centre_col - 2);
-  // g.populate_field(centre_row, centre_col - 2);
 }
 
 int main(int argc, char* argv[]) {
@@ -109,13 +100,11 @@ int main(int argc, char* argv[]) {
   Grid grid(n,n);
   init_grid(grid);
 
-  //TODO: check why diagonal fields are counted as neighbours
   for (int i = 1; i != t; ++i){
-    grid.populate_field(grid.rnd_eligible_field().row,  grid.rnd_eligible_field().col);
+    grid.populate_random_field();
   }
 
-
-  std::cout << grid << "\n";
+  //std::cout << grid << "\n";
 
   return 0;
 }
